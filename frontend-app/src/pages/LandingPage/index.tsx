121d31d3c7e282e2d8eb6d6c4223821caec7d49e
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-05-04 16:48:17 -0300

    add support to css modules

--- frontend-app/src/pages/LandingPage/index.tsx
+++ frontend-app/src/pages/LandingPage/index.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 
+import styles from "./LandingPage.module.css";
+
 import { Link } from "react-router-dom";
+
 
 function LandingPage() {
     return (
-        <>
-            <div>Landing Page</div>
+        <div className={styles.container}>
+            <h1>teste</h1>
             <Link to={"/home"}>navegar para home</Link>
-        </>
+        </div>
     )
 }
 

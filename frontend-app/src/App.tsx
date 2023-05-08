7b2996579bc30780592946a99063f6051c270907
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-05-08 15:28:40 -0300

    create landing page based on figma

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './styles/global.css';
 import Router from './router';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
   return (
-    <Router />
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID!!}>
+        <Router />
+    </GoogleOAuthProvider>
   );
 }
 
--- frontend-app/src/pages/LandingPage/index.tsx
+++ frontend-app/src/pages/LandingPage/index.tsx
@@ -2,14 +2,49 @@ import React from "react";
 
 import styles from "./LandingPage.module.css";
 
-import { Link } from "react-router-dom";
+import AppIcon from '../../assets/app_icon.svg'
+import { GoogleLogin } from "@react-oauth/google";
 
 
 function LandingPage() {
+    console.log(process.env.CLIENT_ID)
+
     return (
         <div className={styles.container}>
-            <h1>teste</h1>
-            <Link to={"/home"}>navegar para home</Link>
+            <div className={styles.gridContainer}>
+                <div className={styles.logoHeader}>
+                    <img src={AppIcon} alt="App icon" width={32} height={32} />
+                    <h1>AFTER CLASS</h1>
+                </div>
+                <div className={styles.appInfo}>
+                    <h1 className={styles.title}>TÍTULO 1</h1>
+                    <h2 className={styles.subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique diam non magna hendrerit.</h2>
+                </div>
+                <div className={styles.loginContainer}>
+                    <p>Crie sua conta agora</p>
+                    <GoogleLogin
+                        containerProps={{
+                            style: {
+                                width: "100%",
+                            }
+                        }}
+                        logo_alignment="center"
+                        shape="pill"
+                        onSuccess={credentialResponse => {
+                            console.log(credentialResponse);
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                    />
+                    <p>ou</p>
+                    <div className={styles.formContainer}>
+                        <input type="text" placeholder="Nome Sobrenome"/>
+                        <input type="text" placeholder="email@email.com" />
+                        <button>CRIAR CONTA</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }

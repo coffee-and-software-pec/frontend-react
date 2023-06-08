fd3fc8a416bf385e98932df46a2c1a10408a55e9
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-06-08 10:24:58 -0300

    add landing publication missing title and disable register with email

--- frontend-app/src/pages/LandingPage/index.tsx
+++ frontend-app/src/pages/LandingPage/index.tsx
@@ -11,6 +11,9 @@ import LandPublication from "../../models/LandPublication";
 import { useAuth } from "../../contexts/AuthContext";
 import { checkAuthToken } from "../../utils/TokenUtil";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function LandingPage() {
     const navigate = useNavigate();
     const [publications, setPublications] = useState<LandPublication[]>([] as LandPublication[]);
@@ -43,6 +46,17 @@ function LandingPage() {
         
     }
 
+    function handleRegisterWithEmail() {
+        toast("Esta opção está desativada", {
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            draggable: false,
+            theme: "light",
+            type: "info"
+        });
+    }
+
     return (
         <div className={styles.container}>
             <div className={styles.gridContainer}>
@@ -51,8 +65,8 @@ function LandingPage() {
                     <h1>AFTER CLASS</h1>
                 </div>
                 <div className={styles.appInfo}>
-                    <h1 className={styles.title}>TÍTULO 1</h1>
-                    <h2 className={styles.subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique diam non magna hendrerit.</h2>
+                    <h1 className={styles.title}>Compartilhe seu know-how</h1>
+                    <h2 className={styles.subtitle}>Leia e escreva publicações sobre temas relacionados a computação. Crie tutoriais, guias ou discussões sobre a nossa área.</h2>
                 </div>
                 <div className={styles.loginContainer}>
                     <p>Crie sua conta agora</p>
@@ -69,7 +83,8 @@ function LandingPage() {
                     <div className={styles.formContainer}>
                         <input type="text" placeholder="Nome Sobrenome"/>
                         <input type="text" placeholder="email@email.com" />
-                        <button>CRIAR CONTA</button>
+                        <button onClick={handleRegisterWithEmail}>CRIAR CONTA</button>
+                        <ToastContainer />
                     </div>
                 </div>
                 <div className={styles.popularPublicationsContainer}>

3304e8f805ae11055f8aff63671f376e7484f18e
davibss <davi.sousa@ccc.ufcg.edu.br>
2023-08-01 20:27:54 -0300

    feat(mobile): add mobile support to landing page

--- frontend-app/src/pages/LandingPage/index.tsx
+++ frontend-app/src/pages/LandingPage/index.tsx
@@ -114,7 +114,7 @@ function LandingPage() {
                     <GoogleLogin
                         logo_alignment="center"
                         shape="pill"
-                        width="300px"
+                        width="auto"
                         onSuccess={handleGoogleSuccessLogin}
                         onError={handleGoogleLoginFailure}
                     />

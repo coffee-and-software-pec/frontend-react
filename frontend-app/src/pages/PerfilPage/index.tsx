323123d1d0520d49ad1c371202ca41a1a6204aaa
FelipeEmanuel <felipe.nunes@ccc.ufcg.edu.br>
2023-08-07 21:18:37 -0300

    Adicionando mais coisas no perfil do usuário

--- frontend-app/src/pages/PerfilPage/index.tsx
+++ frontend-app/src/pages/PerfilPage/index.tsx
@@ -4,11 +4,15 @@ import { useAuth } from '../../contexts/AuthContext';
 import React, { useEffect, useState } from "react"
 import colors from  '../../styles/colorsConfig.json';
 import { ReactComponent as UserIcon } from '../../assets/user_icon.svg';
+import { getUserStatsById } from "../../services/UserService";
+import UserStats from "../../models/UserStats";
+import { getCommentDateTime } from "../../utils/CommentDateUtil";
 
 function PerfilPage() {
 
     const { user, loadUser } = useAuth();
     const [imageLoaded, setImageLoaded] = useState(false);
+    const [statsUser, setStatsUser] = useState<UserStats>();
 
     useEffect(() => {
         const loadedUser = loadUser();
@@ -20,7 +24,20 @@ function PerfilPage() {
             })
             .catch(_ => setImageLoaded(false));
         }
+
+        async function statsUser () {
+            const statsUser = await getUserStatsById(loadedUser.id!!, loadedUser.id!!)
+            setStatsUser(statsUser);
+        }
+
+        statsUser();
+
     }, [])
+
+    function updateUser() {
+        
+    }
+
 
     return (
         <div className={styles.container}>
@@ -42,14 +59,19 @@ function PerfilPage() {
                     <div className={styles.userInfo}>
                         <div className={styles.nome}>
                             <p>Nome</p>
-                            <h1>Felipe</h1>
+                            <input value={statsUser?.name}/>
                         </div>
                         <div className={styles.email}>
                             <p>Email</p>
-                            <h1>felipe@felipe</h1>
+                            <input
+                                value={statsUser?.email}
+                                disabled
+                            />
                         </div>
                         <div className={styles.createButtonLink}>
-                            <button className={styles.sendButton}>
+                            <button 
+                                className={styles.sendButton}
+                                onClick={updateUser}>
                                 SALVAR             
                             </button>
                         </div>                    
@@ -57,21 +79,34 @@ function PerfilPage() {
                     <div className={styles.estatisticas}>
                         <p>Estatísticas</p>
                         <div className={styles.numeros}>
-                            <h1>3</h1>
+                            <h1>{statsUser?.posts}</h1>
                             <h5>Publicações</h5>
-                            <h1>100</h1>
+                            <h1>{statsUser?.likes}</h1>
                             <h5>Likes</h5>
-                            <h1>20</h1>
+                            <h1>{statsUser?.comments}</h1>
                             <h5>Comentários</h5>
-                            <h1>16</h1>
+                            <h1>{statsUser?.followersCount}</h1>
                             <h5>Seguidores</h5>
-                            <h1>8</h1>
+                            <h1>{statsUser?.followingCount}</h1>
                             <h5>Seguindo</h5>
                         </div>
                     </div>
                 </div>
                 <div className={styles.atividades}>
-                    Atividades recentes
+                    <h1>Atividades recentes</h1>
+                    <div className={styles.listaAtividades}>
+                        {imageLoaded ? 
+                            <img       
+                                src={user?.photoURL}
+                                alt=""
+                                referrerPolicy='no-referrer'
+                            /> :
+                            <UserIcon color={colors.theme.white} />
+                        }
+                        {statsUser?.name}
+                        <p>Comentou sua publicação.</p>
+                        {getCommentDateTime("2023-06-11T11:36:28.662931")}            
+                    </div>
                 </div>
             </div>
         </div>
